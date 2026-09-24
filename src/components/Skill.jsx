375100c9a0fe7379ab88938994ed93900c9b893b
suarezsofia541-alt <suarezsofia541@gmail.com>
2026-09-24 16:03:46 -0300

    agrego un array de habilidades y map() en Skills y paso los datos al componente Skill mediante props

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,7 +1,10 @@
-const Skill = () => {
+const Skill = ({nombre, img}) => {
     return(
         <div>
-
+            <div>
+            <h4>{nombre}</h4>
+            <h4>{img}</h4>
+        </div>
         </div>
     )
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,25 @@
+import Skill from './Skill'
+
 const Skills = () => {
+    const habilidades = [
+        { id: 1, nombre: "C#", img:"img"},
+        { id: 2, nombre: "JavaScript", img:"img"},
+        { id: 3, nombre: "MySQL", img:"img"},
+        { id: 4, nombre: "React", img:"img"},
+        { id: 5, nombre: "HTML", img:"img"},
+        { id: 6, nombre: "CSS", img:"img"}
+    ];
+
     return(
         <div id="skills">
             <h2>Habilidades</h2>
+            <div>
+                {habilidades.map((habilidad) => (
+                    <div key={habilidad.id}>
+                        <Skill nombre={habilidad.nombre} img={habilidad.img}/>
+                    </div>
+                ))}  
+            </div>
         </div>
     )
 }
