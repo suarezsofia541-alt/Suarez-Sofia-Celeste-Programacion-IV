fbadc150ffe04d65625c497f6dc75e3de1fb4388
suarezsofia541-alt <suarezsofia541@gmail.com>
2026-09-24 16:14:55 -0300

    agrego un array de proyectos y map() en Projects, pasando los datos al componente Project mediante props, ademas utilizo un estado para controlar la visualizacion de la informacion de los proyectos

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,7 +1,25 @@
-const Project = () => {
+import { useState } from 'react'
+
+const Project = ({nombre,descripcion,tecnologias,img}) => {
+    const [VerMas,setVerMas]=useState(false) 
+
+
     return(
         <div>
-
+            {VerMas ? (
+                <div>
+                    <h4>{nombre}</h4>
+                    <p>Descripcion: {descripcion}</p>
+                    <p>Tecnologias: {tecnologias}</p>
+                    <button onClick={() => setVerMas(false)}>ver menos</button>
+                </div>
+                ) : (
+                <div>
+                    <h4>{img}</h4>
+                    <h4>{nombre}</h4>
+                    <button onClick={() => setVerMas(true)}>ver mas</button>
+                </div>
+            )}
         </div>
     )
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,22 @@
+import Project from './Project'
+
 const Projects = () => {
+    const proyectos = [
+        {id:1, nombre:"TurnoFácil", descripcion:"Sistema web para gestionar turnos de clientes. Permite registrar clientes, consultar horarios disponibles y organizar las reservas.", tecnologias:"C#, HTML, CSS, JavaScript, MySQL.", img:"img"},
+        {id:2, nombre:"ShopNow", descripcion:"Tienda online donde los usuarios pueden ver productos, consultar precios y realizar pedidos de forma sencilla.", tecnologias:"C#, HTML, CSS, JavaScript, MySQL.", img:"img"},
+        {id:3, nombre:"TaskManager", descripcion:"Aplicación web para organizar tareas. Permite agregar, editar, eliminar y marcar tareas como completadas.", tecnologias:"C#, HTML, CSS, JavaScript, MySQL.", img:"img"}
+    ]
+
     return(
         <div id="projects">
             <h2>Proyectos</h2>
+            <div>
+               {proyectos.map((proyecto) => (
+                    <div key={proyecto.id}>
+                        <Project nombre={proyecto.nombre} descripcion={proyecto.descripcion} tecnologias={proyecto.tecnologias} img={proyecto.img}/>
+                    </div>
+                ))} 
+            </div>
         </div>
     )
 }
